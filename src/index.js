c3554f9e7621a65365f28d11f5379c99aba19954
SriJayaVaishnavi <136679689+SriJayaVaishnavi@users.noreply.github.com>
2024-10-25 14:43:15 +0530

    index.js

--- src/index.js
+++ src/index.js
@@ -2,10 +2,9 @@ import React from "react";
 import ReactDOM from "react-dom/client"; 
 import App from "./App.js"; 
 
-const rootElement = document.getElementById("root"); // This should match the id in your HTML
-
+const rootElement = document.getElementById("root");
 if (rootElement) {
-    const root = ReactDOM.createRoot(rootElement); // Create root
+    const root = ReactDOM.createRoot(rootElement); 
     root.render(
         <React.StrictMode>
             <App />

22549299474aa86c5bd62b80318ddec5399b7de4
SriJayaVaishnavi <136679689+SriJayaVaishnavi@users.noreply.github.com>
2024-10-25 14:18:38 +0530

    firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -2,7 +2,7 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getDatabase, ref, set, onValue } from "firebase/database";
 
-// Firebase configuration using environment variables
+
 const firebaseConfig = {
   apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
   authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
@@ -14,27 +14,25 @@ const firebaseConfig = {
   measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
 };
 
-// Initialize Firebase
+
 const app = initializeApp(firebaseConfig);
 const database = getDatabase(app);
 const auth = getAuth(app);
 
-// Function to get sensor data from Firebase
+
 const getSensorsData = () => {
   return new Promise((resolve, reject) => {
     const sensorRef = ref(database, "Sensors");
     onValue(sensorRef, (snapshot) => {
       const data = snapshot.val();
       if (data) {
-        resolve(data); // Resolve the promise with the fetched data
+        resolve(data); 
       } else {
         reject("No sensor data found");
       }
     }, (error) => {
-      reject(error); // Reject the promise if there's an error
+      reject(error); 
     });
   });
 };
-
-// Export the necessary functions and database
 export { auth, database, ref, set, onValue, getSensorsData };
